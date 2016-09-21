dcaef845f9031b3c3ea60d0752494f63b8ec98c9
Swind <swind@cloudmosa.com>
2016-09-21 18:17:26 +0800

    Add appium robot

---
+++ src/appium_robot.py
@@ -0,0 +1,30 @@
+from interface.robot import Robot
+from keycode import ANDROID_KEYCODE
+from cv2img import CV2Img
+from appium.webdriver.common.touch_action import TouchAction
+
+KEYCODE = ANDROID_KEYCODE
+
+class AppiumRobot(Robot):
+    def __init__(self, driver):
+        """
+        :param driver: Appium webdriver
+        """
+        self._driver = driver
+
+    def send_keys(self, keys):
+        for key in keys:
+            self._driver.keyevent(KEYCODE[key])
+
+    def send_key(self, keycode):
+        self._driver.keyevent(keycode)
+
+    def drag_and_drop(self, start_x, start_y, end_x, end_y, duration=None):
+        self._driver.swipe(start_x, start_y, end_x, end_y, duration)
+
+    def capture_screen(self):
+        img = CV2Img()
+        return img.load_base64(self._driver.get_screenshot_as_base64())
+
+    def tap(self, x, y, duration):
+        self._driver.tap([(x, y)], duration)
